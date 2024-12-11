5eb8de72955df40a389afc4fa0e12b110316c3d3
Gloria192 <mutuyegloria@gmail.com>
2024-12-11 06:06:16 -0800

    q5

--- practice/q4.js
+++ practice/q4.js
@@ -1,12 +1,8 @@
-function
-myFunction
-(
-a, n
-)
-{
-
-return a[n-1];
+function myFunction(a, n) {
+    return a[n - 1]; 
 }
-console.log(myFunction('abcd', 1));       
+
+
+console.log(myFunction('abcd', 1));        
 console.log(myFunction('zyxbwpl', 5));     
 console.log(myFunction('gfedcba', 3));
---
+++ practice/q5.js
@@ -0,0 +1,8 @@
+function myFunction(a) {
+    return a.slice(-4);
+}
+
+
+console.log(myFunction('abcdefg'));  
+console.log(myFunction('1234'));     
+console.log(myFunction('fgedcba'))
