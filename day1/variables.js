aa510d8a563ffaa395d57b73188793ed1cb778b4
Gloria192 <mutuyegloria@gmail.com>
2024-12-11 05:08:02 -0800

    line2

--- day1/variables.js
+++ day1/variables.js
@@ -1,11 +1,47 @@
 // variables 
 // const means constant there is no change
 // let is new version of javascript
-let myName="josisne";
-let age=20;
-let  a=12;
-let b=10;
+// data type 
+// underdefined( console.log(typeof(sign))) to call somethong which are not defined
+// String
+// null
 
-console.log(myName);
-console.log(age);
-console.log(a+b);
+// let myName="josisne";
+
+// let  a=12;
+// let b=10;
+// let syl=null;
+// let age=16;
+// let bankacount=600000;
+
+// console.log(myName);
+// console.log(age);
+// console.log(a+b);
+// console.log(typeof(syl))
+// // conditions
+
+// if (age<18){
+// console.log('you are allowes=d to drink')
+// }else{
+//     console,log('you are not allowed to drink')
+// }
+// if(bankacount>=1000000){
+// console.log('you rich')
+// }else if (bankaccount>=5000000){
+//     console.log('you good')
+// }else{
+//     console.log('you broke')
+// }
+let language='english';
+if(language=='muraho'){
+    console.log('the country is Rwanda')
+
+}else if(language=='english')
+{
+console.log('the country is uk')
+}
+else if(language=='swahil'){
+console.log('THE country is tanzania')
+}else {
+    console.log('There is no country')
+}
