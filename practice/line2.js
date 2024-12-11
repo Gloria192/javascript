b52703614828a024ebfb6faaff719c72eb743ca1
Gloria192 <mutuyegloria@gmail.com>
2024-12-11 05:53:16 -0800

    three

---
+++ practice/line2.js
@@ -0,0 +1,6 @@
+function myFunction(a, b) {
+    return a === b;
+}
+console.log(myFunction(2, 3));        
+console.log(myFunction(1, '1'));      
+console.log(myFunction('10', '10'));  
---
+++ practice/q4.js
@@ -0,0 +1,12 @@
+function
+myFunction
+(
+a, n
+)
+{
+
+return a[n-1];
+}
+console.log(myFunction('abcd', 1));       
+console.log(myFunction('zyxbwpl', 5));     
+console.log(myFunction('gfedcba', 3));
