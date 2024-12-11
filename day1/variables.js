05db471921e73468a2be50ef3bd3f3e47c1ecfe2
Gloria192 <mutuyegloria@gmail.com>
2024-12-11 00:45:04 -0800

    first

---
+++ day1/variables.js
@@ -0,0 +1,11 @@
+// variables 
+// const means constant there is no change
+// let is new version of javascript
+let myName="josisne";
+let age=20;
+let  a=12;
+let b=10;
+
+console.log(myName);
+console.log(age);
+console.log(a+b);
